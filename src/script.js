ae26d898c90a364e78ccb7957c6ddac33b25baee
Ryan Hall <dillbc@protonmail.com>
2023-07-18 16:12:44 -0400

    2nd submission with assumed true conditions condensed and extended challenges completed

--- src/script.js
+++ src/script.js
@@ -2,21 +2,29 @@
 
 // TODO - write your code here.
 
+// # 1
 const randomDamage = () => Math.floor(Math.random() * 10 + 1);
+
+// #2
 const chooseOption = (opt1, opt2) => {
   const randNum = Math.floor(Math.random() * 2);
-  return randNum === 0 ? opt1 : opt2;
+  return randNum ? opt2 : opt1;
 };
 
+// #3
 const attackPlayer = (health) => health - randomDamage();
 
+// #4
 const logHealth = (player, health) =>
   console.log(`${player} health: ${health}`);
 
+// #5
 const logDeath = (winner, loser) => console.log(`${winner} defeated ${loser}`);
 
-const isDead = (health) => (health <= 0 ? true : false);
+// #6
+const isDead = (health) => health <= 0;
 
+// #7
 const fight = (player1, player2, player1Health, player2Health) => {
   while (true) {
     const attacker = chooseOption(player1, player2);
@@ -38,4 +46,49 @@ const fight = (player1, player2, player1Health, player2Health) => {
   }
 };
 
-fight("Ryan", "Raul", 100, 100);
+// #8
+fight("Ryan", "Paul", 100, 100);
+
+// #Extended Challenges
+const getGrade = (number) => {
+  if (number >= 90) {
+    return "A";
+  } else if (number >= 80) {
+    return "B";
+  } else if (number >= 70) {
+    return "C";
+  } else if (number >= 60) {
+    return "D";
+  } else if (number < 60) {
+    return "F";
+  }
+};
+
+console.log(getGrade(Math.floor(Math.random() * 100 + 1)));
+
+const prioritize = (urgent, important) => {
+  if (urgent && important) {
+    return 1;
+  } else if (!urgent && important) {
+    return 2;
+  } else if (urgent && !important) {
+    return 3;
+  } else if (!urgent && !important) {
+    return 4;
+  }
+};
+
+console.log(prioritize(true, true));
+
+const calculatePay = (wage, hours) => {
+  if (hours <= 40) {
+    return wage * hours;
+  } else {
+    const overtimeHours = hours - 40;
+    const overtimePay = overtimeHours * (wage * 1.5);
+    const regularPay = wage * (hours - overtimeHours);
+    return regularPay + overtimePay;
+  }
+};
+
+console.log(calculatePay(10, 50));
